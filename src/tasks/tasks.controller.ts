9bbee7404537b69f9d7e7051a9f6f9ec6c21d791
yuriigeivakh <yurageyvakh@gmail.com>
2020-04-11 17:38:26 +0300

    Update task status

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,6 +1,6 @@
-import { Controller, Get, Post, Body, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Param, Delete, Patch } from '@nestjs/common';
 import { TasksService } from './tasks.service';
-import { Task } from './task.model';
+import { Task, TaskStatus } from './task.model';
 import { CreateTaskDTO } from './dto/create-task-dto';
 
 @Controller('tasks')
@@ -20,6 +20,14 @@ export class TasksController {
     @Post()
     createTask(@Body() createTaskDTO: CreateTaskDTO): Task {
         return this.tasksService.createTask(createTaskDTO);
+    }
+
+    @Patch('/:id')
+    updateTaskStatus(
+        @Param('id') id: string,
+        @Body() status: TaskStatus,
+    ): Task {
+        return this.tasksService.updateTaskStatus(id, status);
     }
 
     @Delete('/:id')
