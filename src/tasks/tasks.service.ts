03261e7102d98708fabb692b8afe57b32b6703af
yuriigeivakh <yurageyvakh@gmail.com>
2020-04-11 16:21:51 +0300

    Create a task

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,5 +1,7 @@
+import { v1 as uuidv1 } from 'uuid';
 import { Injectable } from '@nestjs/common';
-import { Task } from './task.model';
+import { Task, TaskStatus } from './task.model';
+import { CreateTaskDTO } from './dto/create-task-dto';
 
 @Injectable()
 export class TasksService {
@@ -7,5 +9,18 @@ export class TasksService {
 
     getAllTasks(): Task[] {
         return this.tasks;
+    }
+
+    createTask(createTaskDTO: CreateTaskDTO): Task {
+        const { title, description } = createTaskDTO;
+        const task: Task = {
+            id: uuidv1(),
+            title,
+            description,
+            status: TaskStatus.OPEN,
+        }
+
+        this.tasks.push(task);
+        return task;
     }
 }
