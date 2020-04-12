f81d7bec068f59d553be56ad78ea0d4a0768ef9f
yuriigeivakh <yurageyvakh@gmail.com>
2020-04-12 08:56:38 +0300

    Validation Task Status

---
+++ src/tasks/pipes/task-status-validation.pipe.ts
@@ -0,0 +1,20 @@
+import { PipeTransform, BadRequestException } from "@nestjs/common";
+import { TaskStatus } from "../task.model";
+
+export class TaskStatusValidationPipe implements PipeTransform {
+    readonly allowedStatuses = [TaskStatus.DONE, TaskStatus.IN_PROGRESS, TaskStatus.OPEN];
+    transform(value: any) {
+        value = value.toUpperCase();
+
+        if (!this.isStatusValid(value)) {
+            throw new BadRequestException(`${value} is an invalid status`);
+        }
+        
+        return value
+    }
+
+    private isStatusValid(status: any) {
+        const index = this.allowedStatuses.indexOf(status);
+        return index !== -1;
+    }
+}
