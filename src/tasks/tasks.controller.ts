94736011c1e5e13b4d4600a03c86bca9ec636997
yuriigeivakh <yurageyvakh@gmail.com>
2020-04-13 10:44:44 +0300

    Persistence: update task status

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -4,6 +4,7 @@ import { CreateTaskDTO } from './dto/create-task-dto';
 import { GetTasksFilterDTO } from './dto/get-tasks-filter-dto';
 import { TaskStatusValidationPipe } from './pipes/task-status-validation.pipe';
 import { Task } from './task.entity';
+import { TaskStatus } from './task-status.enum';
 
 @Controller('tasks')
 export class TasksController {
@@ -30,13 +31,13 @@ export class TasksController {
         return this.tasksService.createTask(createTaskDTO);
     }
 
-    // @Patch('/:id')
-    // updateTaskStatus(
-    //     @Param('id') id: string,
-    //     @Body('status', TaskStatusValidationPipe) status: TaskStatus,
-    // ): Task {
-    //     return this.tasksService.updateTaskStatus(id, status);
-    // }
+    @Patch('/:id')
+    updateTaskStatus(
+        @Param('id', ParseIntPipe) id: number,
+        @Body('status', TaskStatusValidationPipe) status: TaskStatus,
+    ): Promise<Task> {
+        return this.tasksService.updateTaskStatus(id, status);
+    }
 
     @Delete('/:id')
     deleteTask(@Param('id', ParseIntPipe) id: number): Promise<void> {
