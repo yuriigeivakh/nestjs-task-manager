d6f44ce0b52882356b697e06e0fd1951bcf917ca
yuriigeivakh <yurageyvakh@gmail.com>
2020-04-16 07:45:19 +0300

    Authorization: update a user's task status

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -39,13 +39,14 @@ export class TasksController {
         return this.tasksService.createTask(createTaskDTO, user);
     }
 
-    // @Patch('/:id')
-    // updateTaskStatus(
-    //     @Param('id', ParseIntPipe) id: number,
-    //     @Body('status', TaskStatusValidationPipe) status: TaskStatus,
-    // ): Promise<Task> {
-    //     return this.tasksService.updateTaskStatus(id, status);
-    // }
+    @Patch('/:id')
+    updateTaskStatus(
+        @Param('id', ParseIntPipe) id: number,
+        @Body('status', TaskStatusValidationPipe) status: TaskStatus,
+        @GetUser() user: User,
+    ): Promise<Task> {
+        return this.tasksService.updateTaskStatus(id, status, user);
+    }
 
     @Delete('/:id')
     deleteTask(@Param('id', ParseIntPipe) id: number): Promise<void> {
